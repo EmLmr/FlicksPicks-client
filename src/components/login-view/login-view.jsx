640691f6d443bdf1625c7c55604f9530daf789ee
EmLmr <emeline.coding@gmail.com>
2021-08-25 16:33:54 +0200

    added bootstrap and styled login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 export function LoginView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -8,25 +13,34 @@ export function LoginView(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
+    //send a request to the server for authentication
+    //then call props.onLoggedIn(username)
     props.onLoggedIn(username);
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>
-        Login
-      </button>
-    </form>
+    <Form>
+      <Row className="form-body justify-content-center">
+        <Col xs={8} md={6}>
+          <Form.Group controlId="formUsername">
+            <Form.Label>Username:</Form.Label>
+            <Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+          </Form.Group>
+
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password:</Form.Label>
+            <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-center">
+          <Button type="submit" size="lg" bsPrefix="submit-btn" onClick={handleSubmit}>
+            Login
+          </Button>
+        </Col>
+      </Row>
+    </Form>
   );
 }
 
